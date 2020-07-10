8edc49acb8506ab5dc7bbfb8136c294527fa3c3d
Prasenjit <dprasen@gmail.com>
2020-07-10 23:18:05 +0530

    Updates

---
+++ precache-manifest.b9993e0abbf579dece501c2e176b4ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab7e2286b2737064ea6476bb197aab6",
+    "url": "/dprasen.github.io/index.html"
+  },
+  {
+    "revision": "d93ca14472ee173b23ba",
+    "url": "/dprasen.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3b6bb12578f350e3eab",
+    "url": "/dprasen.github.io/static/js/2.f5bf7272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dprasen.github.io/static/js/2.f5bf7272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93ca14472ee173b23ba",
+    "url": "/dprasen.github.io/static/js/main.f79a3635.chunk.js"
+  },
+  {
+    "revision": "8f9fc65954304cfddeff",
+    "url": "/dprasen.github.io/static/js/runtime-main.fff348e6.js"
+  }
+]);
